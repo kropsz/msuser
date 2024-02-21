63a49e3fcc8025c7ef8aedd1ea535c29423290da
Pedro Esteves <kropinho211@outlook.com>
2024-02-21 16:19:34 -0300

    Fix: Correção no campo birthDate

--- src/main/java/com/compassuol/sp/challenge/msuser/web/dto/UserRequestDto.java
+++ src/main/java/com/compassuol/sp/challenge/msuser/web/dto/UserRequestDto.java
@@ -6,7 +6,8 @@ import org.hibernate.validator.constraints.br.CPF;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
@@ -22,10 +23,10 @@ public class UserRequestDto {
     @CPF
     @Size(min = 11, max = 11)
     private String cpf;
-    @NotBlank(message = "Campo data de nascimento não pode ser vazio")
-    @Pattern(regexp = "[0-9]{4}-[0-9]{2}-[0-9]{2}", message = "Formato da data de nascimento está errado")
+    @NotNull(message = "Campo data de nascimento não pode ser vazio")
+    @Past
     private Date birthdate;
-    @NotBlank(message = "Campo 'email' não pode ser vazio")
+    @NotNull(message = "Campo 'email' não pode ser vazio")
     @Email(message = "Formato do e-mail está invalido", regexp = "^[a-z0-9.+-]+@[a-z0-9.-]+\\.[a-z]{2,}$")
     private String email;
     @NotBlank(message = "Campo'password' não pode ser vazio")
